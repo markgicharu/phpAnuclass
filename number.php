8bc10890258f4c738a0208428b13cbe9edd452a0
markgicharu <markgicharu@hotmail.com>
2016-05-30 19:28:58 +0300

    Update number.php

--- number.php
+++ number.php
@@ -7,9 +7,13 @@
    <?php
    $numbers = array(4,8,15,16,23,42);
    echo $numbers[1]; 
-  $mixed = array(9,"fox","dog",array("x","y","z"));
+   $mixed = array(9,"fox","dog",array("x","y","z"));
+   <pre>
    echo $mixed[2];
+   </pre>
+   <pre>
    echo $mixed[3];
+   </pre>
    echo print_r($mixed);
    $mixed[2]="cat";
    $mixed[4]="mouse";
