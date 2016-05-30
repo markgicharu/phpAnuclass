15dd3d0abd5f2f5590cd99643dd52ee821025c16
markgicharu <markgicharu@hotmail.com>
2016-05-30 19:12:16 +0300

    Update number.php

--- number.php
+++ number.php
@@ -1,11 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html>
+   <head>
+    <title>numbers</title>  
+   </head>
+   <body>
+      <?php
    $numbers = array(4,8,15,16,23,42);
-echo $numbers[1]; 
-$mixed = array(9,"fox", "dog", array("x,""y","z"));
-echo$mixed[2];
-echo$mixed[3];
-echo print_r($mixed);
-$mixed[2]="cat";
-$mixed[4]="mouse";
-$mixed[1]="horse";
+   echo $numbers[1]; 
+   $mixed = array(9,"fox", "dog", array("x,""y","z"));
+   echo$mixed[2];
+   echo$mixed[3];
+   echo print_r($mixed);
+   $mixed[2]="cat";
+   $mixed[4]="mouse";
+   $mixed[1]="horse";
 >
+      
+   </body>
+</html>
+
